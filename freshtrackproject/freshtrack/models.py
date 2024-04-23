7d8d82db5399e61baf7eaee2f4fb27d5d3444bea
Davide <davidegalbiati29@gmail.com>
2024-04-23 22:32:49 +0200

    Added the class user

--- freshtrackproject/freshtrack/models.py
+++ freshtrackproject/freshtrack/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    firstname = models.CharField(max_length=100)  # Specify the max_length attribute
+    lastname = models.CharField(max_length=100)  # Specify the max_length attribute
+    email = models.EmailField()
+    password = models.CharField(max_length=100)   # Specify the max_length attribute
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.firstname + ' ' + self.lastname
+    
