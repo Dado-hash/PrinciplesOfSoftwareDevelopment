98de3802306eb940b0e8d120c4d2097b7aaa740e
Davide <davidegalbiati29@gmail.com>
2024-04-23 22:07:35 +0200

    Added admin page

--- freshtrackproject/freshtrackproject/urls.py
+++ freshtrackproject/freshtrackproject/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.urls import path
 from freshtrack.views import index, login
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('login', login, name='login')
 ]
