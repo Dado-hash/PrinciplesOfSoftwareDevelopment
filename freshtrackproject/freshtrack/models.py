bff9178f747f677d91bf93e285b602a1034923d8
Davide <davidegalbiati29@gmail.com>
2024-04-23 23:16:06 +0200

    Created a register page

--- freshtrackproject/freshtrack/models.py
+++ freshtrackproject/freshtrack/models.py
@@ -6,8 +6,6 @@ class User(models.Model):
     lastname = models.CharField(max_length=100)  # Specify the max_length attribute
     email = models.EmailField()
     password = models.CharField(max_length=100)   # Specify the max_length attribute
-    age = models.IntegerField()
 
     def __str__(self):
         return self.firstname + ' ' + self.lastname
-    
--- freshtrackproject/freshtrackproject/urls.py
+++ freshtrackproject/freshtrackproject/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from freshtrack.views import index, login
+from freshtrack.views import index, login, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('login', login, name='login')
+    path('login', login, name='login'),
+    path('register', register, name='register')
 ]
